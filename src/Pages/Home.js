ccb5b0b51c76f8f0525709638d77e22b9b72a7ea
HarshPrajapati2003 <hrsdp2683@gmail.com>
2023-07-31 15:47:31 +0530

    First Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../features/Navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+const Home = () => {
+  return (
+    <div>
+        <Navbar>
+        <ProductList/>
+        </Navbar>
+    </div>
+  )
+}
+
+export default Home
