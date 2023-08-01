58a76bdb40438cefa4e54788740068b98f0c8e3e
HarshPrajapati2003 <hrsdp2683@gmail.com>
2023-08-01 18:26:18 +0530

    Third Commit
    Set up redux, sorting and filter functionality

--- src/Pages/ProductDetailPage.js
+++ src/Pages/ProductDetailPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProductDetail from '../features/product-list/components/ProductDetail'
+import ProductDetail from '../features/product/components/ProductDetail'
 import Navbar from '../features/Navbar/Navbar'
 
 const ProductDetailPage = () => {
---
+++ src/features/product/ProductSlice.js
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchAllProducts,fetchProductsByFilters } from './ProductAPI';
+
+const initialState = {
+  products : [],
+  status: 'idle',
+};
+
+export const fetchAllProductsAsync = createAsyncThunk(
+  'product/fetchAllProductsAsync',
+  async () => {
+    const response = await  fetchAllProducts();
+    return response.data;
+  }
+);
+
+export const fetchAllProductsByFilterAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async (filter) => {
+    const response = await  fetchProductsByFilters(filter);
+    return response.data;
+  }
+);
+
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProductsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
+      })
+      .addCase(fetchAllProductsByFilterAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllProductsByFilterAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
+      });
+  },
+});
+
+export const { increment} = productSlice.actions;
+
+
+export const selectAllProducts = (state) => state.product.products;
+
+export default productSlice.reducer;
