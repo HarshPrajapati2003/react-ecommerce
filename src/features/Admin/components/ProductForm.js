a4a7d28da39bb1630bf65e9616f80ee1f48579f9
HarshPrajapati2003 <hrsdp2683@gmail.com>
2023-08-08 23:30:54 +0530

    16th Commit
    Small Error Fix in Product Form

--- src/features/Admin/components/ProductForm.js
+++ src/features/Admin/components/ProductForm.js
@@ -409,8 +409,7 @@ const ProductForm = () => {
           Cancel
         </Link>
         {selectedProduct && <button
-          type="submit"
-          onClick={(e)=>{errors.preventDefault();setOpenModal(true)}}
+          onClick={(e)=>{e.preventDefault();setOpenModal(true)}}
           className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
         >
           Delete
